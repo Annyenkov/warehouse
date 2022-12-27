69f0155d2eb6c48ec52be483f2b025157382f249
Viktor Annyenkov <annyenkov@gmail.com>
2022-12-27 23:55:14 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import SharedLayout from "components/SharedLayout/SharedLayout";
 import { lazy } from "react";
 
-const Movies = lazy(() => import('../services/pages/Movies'));
-const Home = lazy(() => import('../services/pages/Home'));
-const MovieDetails = lazy(() => import('../services/pages/MovieDetails'));
+const Movies = lazy(() => import('../pages/Movies'));
+const Home = lazy(() => import('../pages/Home'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('./Cast'));
 const Reviews = lazy(() => import('./Reviews'));
-const NotFound = lazy(() => import('../services/pages/NotFound'));
+const NotFound = lazy(() => import('../pages/NotFound'));
 
 
 const App = () => {
